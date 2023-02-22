c0535fc63d4011e7324ec6e89642475647d5b7e4
“김주성” <dlrtls12345@naver.com>
2023-02-22 20:56:50 +0900

    js

--- src/baekjoon/BOJ10800.java
+++ src/baekjoon/BOJ10800.java
@@ -16,7 +16,8 @@ public class BOJ10800 {
 
     static int N, totalSize;
     static Ball[] balls;
-    static int[] sizeByColor, answer;
+    static int[] sizeByColor, countBySize, answer;
+    static Map<?, ?>[] countByColorBySize;
     static StringBuilder sb;
 
     public static void main(String[] args) throws IOException {
@@ -25,6 +26,8 @@ public class BOJ10800 {
         N = Integer.parseInt(br.readLine());
         balls = new Ball[N];
         sizeByColor = new int[N + 1];
+        countBySize = new int[2001];
+        countByColorBySize = new HashMap[2001];
         answer = new int[N];
         sb = new StringBuilder();
 
@@ -35,15 +38,27 @@ public class BOJ10800 {
             balls[i] = new Ball(i, color, size);
             totalSize += size;
             sizeByColor[color] += size;
+            countBySize[size]++;
+            Map<Integer, Integer> countByColor = (Map<Integer, Integer>) countByColorBySize[size];
+            if (countByColor == null) {
+                countByColor = new HashMap<>();
+            }
+            Integer val = countByColor.get(color);
+            countByColor.put(color, val == null ? 1 : val + 1);
         }
 
         Arrays.sort(balls, (ball1, ball2) -> ball2.size - ball1.size);
 
         for (int i = 0; i < N; i++) {
             Ball cur = balls[i];
-            answer[cur.number] = totalSize - sizeByColor[cur.color];
+            Map<Integer, Integer> map = ((Map<Integer, Integer>)countByColorBySize[cur.size]);
+            Integer count = map.get(cur.color);
+            int countSameSizeByOtherColor = countBySize[cur.size] - count;
+            answer[cur.number] = totalSize - sizeByColor[cur.color] - countSameSizeByOtherColor * cur.size;
             totalSize -= cur.size;
             sizeByColor[cur.color] -= cur.size;
+            countBySize[cur.size]--;
+            map.put(cur.color, count - 1);
         }
 
         for (int i = 0; i < N; i++) {
