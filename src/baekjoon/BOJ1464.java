c74b18244752f86642afd4381f4f18ded3c0cfa6
“김주성” <dlrtls12345@naver.com>
2023-02-28 00:14:04 +0900

    js

--- src/baekjoon/BOJ1464.java
+++ src/baekjoon/BOJ1464.java
@@ -4,34 +4,29 @@ import java.io.*;
 
 public class BOJ1464 {
 
+    static String s, reverse;
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
+        s = br.readLine();
 
         for (int i = 2; i < s.length(); i++) {
 
-            String tmp = s.substring(0, i);
+            String sub = s.substring(0, i);
+            reverse = new StringBuilder(sub).reverse().toString();
+
+            boolean isReverse1 = (sub.compareTo(reverse) < 0) && (sub.charAt(0) >= s.charAt(i));
+            boolean isReverse2 = (sub.compareTo(reverse) > 0) && (sub.charAt(i - 1) < s.charAt(i));
 
             // 뒤집어야 하는 경우
-            if (((tmp.compareTo(reverse(tmp)) < 0) && (tmp.charAt(0) >= s.charAt(i)))
-                    || ((tmp.compareTo(reverse(tmp)) > 0) && (tmp.charAt(i - 1) < s.charAt(i)))
-            ) {
-                s = reverse(tmp) + s.substring(i);
+            if (isReverse1 || isReverse2) {
+                s = reverse + s.substring(i);
             }
         }
 
-        if (s.compareTo(reverse(s)) < 0) {
-            System.out.print(s);
-        } else {
-            System.out.print(reverse(s));
-        }
-    }
-
-    static String reverse(String s) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = s.length() - 1; i >= 0; i--)
-            sb.append(s.charAt(i));
-        return sb.toString();
+        reverse = new StringBuilder(s).reverse().toString();
+        String answer = s.compareTo(reverse) < 0 ? s : reverse;
+        System.out.println(answer);
     }
 }
